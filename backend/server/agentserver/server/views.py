408957368afbe2889c0a6842ad35e5d24a7fb6c9
may_i21 <may_i21@163.com>
2023-07-15 14:03:27 +0800

    change cron

--- backend/server/agentserver/server/views.py
+++ backend/server/agentserver/server/views.py
@@ -1,5 +1,7 @@
+from apscheduler.triggers.interval import IntervalTrigger
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 from .models import User, Instance
@@ -17,3 +19,32 @@ def get_user(request):
     data = User.objects.all()
     return HttpResponse(data)
 
+
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
+
+# 实例化调度器
+scheduler = BackgroundScheduler()
+# 调度器使用默认的DjangoJobStore()
+scheduler.add_jobstore(DjangoJobStore(), 'default')
+
+def health_monitor():
+    instances = Instance.objects.all()
+    for ins in instances:
+        health_check_url = "http://{}:{}/health".format(ins.ip, ins.server_port)
+        print(health_check_url)
+        resp = requests.get(health_check_url)
+        print(resp.status_code)
+        if resp.status_code == 200:
+            ins.status = True
+            ins.save()
+
+
+scheduler.add_job(health_monitor,
+                  trigger=IntervalTrigger(seconds=5),
+                  id='health_check',
+                  name='health check')
+
+
+scheduler.start()
