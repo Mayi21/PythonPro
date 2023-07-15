8ee26243f56f0e6ad83e514c77905e311a8b426f
may_i21 <may_i21@163.com>
2023-07-15 12:38:07 +0800

    add instance model

--- backend/server/agentserver/server/models.py
+++ backend/server/agentserver/server/models.py
@@ -9,3 +9,17 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Instance(models.Model):
+    '''
+    ip: instance ip
+    status: true is online and false is offline
+    server_port: running port of agent client in client host
+    last_update_time: last time of health check
+    '''
+    ip = models.CharField(max_length=15)
+    status = models.BooleanField()
+    server_port = models.IntegerField()
+    last_update_time = models.DateTimeField()
+
