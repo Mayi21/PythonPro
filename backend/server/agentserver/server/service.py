45c3efb618d4b746e958856c0c8ffb8209483a01
may_i21 <may_i21@163.com>
2023-07-16 16:15:00 +0800

    add service

---
+++ backend/server/agentserver/server/service.py
@@ -0,0 +1,14 @@
+import subprocess
+
+from django.http import JsonResponse
+
+
+def get_command_res(request):
+    try:
+        command = request.POST.get('cmd')
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        result = output.decode().strip() if output else error.decode().strip()
+        return JsonResponse({"result": result})
+    except Exception as e:
+        return JsonResponse({"result": str(e)})
