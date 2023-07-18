646734c3be4094de666b40dc07bf80b72d00b770
may_i21 <may_i21@163.com>
2023-07-18 23:04:56 +0800

    delete no-use file

--- frontend/models.py
+++
@@ -1,9 +0,0 @@
-from tortoise import fields, models
-
-
-class User(models.Model):
-    id = fields.BigIntField(pk=True)
-    username = fields.CharField(max_length=20)
-
-    class Meta:
-        table = "users"
