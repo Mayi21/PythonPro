a8bd2ffbb9fa990011dc814d4435f1b4e489931e
may_i21 <may_i21@163.com>
2023-07-19 22:53:36 +0800

    add fake data func and get data func

--- agentserver/server/tests.py
+++ agentserver/server/tests.py
@@ -1,3 +1,13 @@
+import random
+
 from django.test import TestCase
 
 # Create your tests here.
+
+
+ips = []
+with open('ip.txt', 'rb') as f:
+    lines = f.readlines()
+    for line in lines:
+        ips.append(str(line).strip().replace('b\'', '').replace('\\n\'', ''))
+print(ips)
