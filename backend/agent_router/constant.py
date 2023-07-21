7dd43a0714cd0de754c9426a1202ffa9827cbf94
may_i21 <may_i21@163.com>
2023-07-21 22:17:36 +0800

    add constant

--- backend/agent_router/constant.py
+++ backend/agent_router/constant.py
@@ -4,3 +4,7 @@ from enum import Enum
 class KafkaInfo(Enum):
     COMMAND_TOPIC = "server_command"
     RES_TOPIC = "client_res"
+
+
+class InstanceEnv(Enum):
+    PLUGIN_SCRIPT_PATH = "/opt/plugin"
