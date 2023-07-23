85cd2399858ec5c77f76430e261e14d985759c69
may_i21 <may_i21@163.com>
2023-07-23 16:34:39 +0800

    add test uplaod file

--- backend/agent_router/test.py
+++ backend/agent_router/test.py
@@ -2,6 +2,7 @@ import getopt
 import sys
 import uuid
 
+import requests
 
 
 def main(argv):
@@ -16,5 +17,14 @@ def main(argv):
             bootstrap_servers = arg
     print(bootstrap_servers)
 
+
+def test_upload_file():
+    files = {'file': open('temp/test1.jpeg', 'rb')}
+    resp = requests.post('http://127.0.0.1:8000/uploadfiles', files=files)
+    if resp.status_code == 200:
+        print("upload successful")
+    else:
+        print("failed to upload")
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+   test_upload_file()
