8193ae769c773b44336968875b3420a0ad723419
may_i21 <may_i21@163.com>
2023-07-24 22:35:07 +0800

    change save folder

--- backend/agent_router/agent_server.py
+++ backend/agent_router/agent_server.py
@@ -11,6 +11,7 @@ from fastapi import FastAPI
 from slowapi.errors import RateLimitExceeded
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
+from .constant import InstanceEnv
 
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
@@ -48,9 +49,6 @@ async def execute_shell_file(file_name: str):
 @app.post('/uploadfiles')
 async def upload_shell_file(file: UploadFile=File(...)):
     # need a front page that
-    # 1.must end with .sh (optional)
-    # 2.check size (must)
-    # 3.rate limitation  (must)
     file_data = file.file.read()
     if file.size / 1024 >= 1024:
         return {'error': 'shell script to large'}
@@ -58,14 +56,11 @@ async def upload_shell_file(file: UploadFile=File(...)):
     if not file_name.endswith(".sh"):
         return {'error': 'file check not match'}
     print(file_name)
-    # 1.save to the specified folder
-    # 2.consider duplicate file name
-    # for transport with name and attr,save in local temp dir then send to des from local
-    with open(file_name, 'wb') as f:
+    des_file = os.path.join(InstanceEnv.PLUGIN_SCRIPT_PATH.value, file_name)
+    with open(des_file, 'wb') as f:
         f.write(file_data)
 
-
-    return {"filename": file.filename}
+    return {"success": file.filename}
 
 
 # file upload test
