bac77f8a7373aa49a0557e0b83cb26eff48aa501
may_i21 <may_i21@163.com>
2023-07-26 19:42:16 +0800

    handle fastapi cors

--- backend/agent_router/agent_server.py
+++ backend/agent_router/agent_server.py
@@ -5,6 +5,7 @@ from fastapi import UploadFile, File
 from fastapi import FastAPI, Request
 from fastapi_limiter import FastAPILimiter
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse
 from constant import InstanceEnv
 from fastapi import FastAPI
@@ -15,6 +16,16 @@ from .constant import InstanceEnv
 
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+origins = [
+    "http://localhost:8080",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
