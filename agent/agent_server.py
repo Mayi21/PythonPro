037b517808f402258c60975d7f3c9f2ad89ca298
may_i21 <may_i21@163.com>
2023-07-30 20:51:28 +0800

    update agent_server.py

--- agent/agent_server.py
+++ agent/agent_server.py
@@ -10,6 +10,7 @@ from slowapi.errors import RateLimitExceeded
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from constant import InstanceEnv
+import socket
 
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
@@ -35,7 +36,6 @@ class Cmd(BaseModel):
 # health api
 @app.get("/health")
 async def get_info():
-    import socket
 
     # 获取本机计算机名称
     hostname = socket.gethostname()
