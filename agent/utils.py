7cd78f380caf622c7a00d7e3f315410fb3507bfa
may_i21 <may_i21@163.com>
2023-07-30 21:56:59 +0800

    update exec command func

--- agent/utils.py
+++ agent/utils.py
@@ -1,5 +1,15 @@
 import os
+import subprocess
 
+
+def __exec_cmd(cmd):
+    try:
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        result = output.decode().strip() if output else error.decode().strip()
+        return {"result": result}
+    except Exception as e:
+        return {"result": str(e)}
 
 def check_shell_file(file_name):
 
