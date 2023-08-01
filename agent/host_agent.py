4ed8a496c48cd54f122e42338fce5f4c051d5550
may_i21 <may_i21@163.com>
2023-08-01 23:21:27 +0800

    fix import package

--- agent/host_agent.py
+++ agent/host_agent.py
@@ -10,7 +10,7 @@ from slowapi.errors import RateLimitExceeded
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 
-from agent.utils import __exec_cmd
+from utils import __exec_cmd
 from constant import InstanceEnv
 
 limiter = Limiter(key_func=get_remote_address)
