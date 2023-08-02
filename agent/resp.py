38a0c33c5c92a04428b64c92316a77fe4a4f232d
may_i21 <may_i21@163.com>
2023-08-02 20:09:44 +0800

    update resp

--- agent/resp.py
+++ agent/resp.py
@@ -3,9 +3,8 @@ from constant import RespCode
 
 class Response:
     code: RespCode
-    msg: dict
+    msg: str
 
-
-
-
-
+    def __init__(self, code: RespCode.SUCCESS_CODE, msg: str):
+        self.code = code
+        self.msg = msg
