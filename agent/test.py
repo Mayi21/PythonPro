ae43c3933fa8ede98eb0a1af5065e88295fa26e4
may_i21 <may_i21@163.com>
2023-08-06 18:37:21 +0800

    change deploy host api

--- agent/test.py
+++ agent/test.py
@@ -31,6 +31,7 @@ def test_upload_file():
     else:
         print("failed to upload")
 
+
 def deploy_host(port: str):
     out = __exec_cmd('docker run -d -p {}:8000 --name agent_{} agent'.format(port, port))
     if len(out['result']) != 64:
@@ -45,6 +46,7 @@ def stop_host(container_id: str):
     return {'success': out['result']}
     pass
 
+
 def del_host(container_id: str):
     out = __exec_cmd('docker container rm {}'.format(container_id))
     if len(out['result']) != 64:
@@ -52,11 +54,28 @@ def del_host(container_id: str):
     return {'success': out['result']}
 
 
-if __name__ == "__main__":
-   # print(deploy_host('8081'))
-   #  print(del_host('d2aafd335ca9ea87c73e3e467efa03a26a5dd9fba65a30e49fa32079a5b65ae9'))
-   # print(stop_host('a4d5ddc33b320c7571043679fb3eba6cc9b6b91165549d7ddbe8dd867330acc4'))
+def sync_vm_info():
+    out = __exec_cmd(DockerCMD.GET_RUNNING_VM.value)['result']
+    out = out.split("\n")
+    if len(out) == 1:
+        return
+    vm_infos = []
+    for info in out[1:]:
+        id = info[:12]
+        out = __exec_cmd("{} {}".format(DockerCMD.GET_VM_PORT.value,
+                                        id))['result']
+        port = out.split("->")[-1].split(":")[-1]
+        vm_infos.append({'id': id,
+                         'port': port})
 
-   # print(DockerCMD.DEL_VM.value)
-   # print('200'== RespCode.SUCCESS_CODE.value)
-   print(DockerCMD.GET_IP.value)
+    return vm_infos
+
+
+if __name__ == "__main__":
+    # print(deploy_host('8081'))
+    #  print(del_host('d2aafd335ca9ea87c73e3e467efa03a26a5dd9fba65a30e49fa32079a5b65ae9'))
+    # print(stop_host('a4d5ddc33b320c7571043679fb3eba6cc9b6b91165549d7ddbe8dd867330acc4'))
+
+    # print(DockerCMD.DEL_VM.value)
+    # print('200'== RespCode.SUCCESS_CODE.value)
+    print(sync_vm_info())
