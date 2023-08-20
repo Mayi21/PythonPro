c2fe22268ff2d54e059a0880336ec8ecb25968fd
may_i21 <may_i21@163.com>
2023-08-20 21:20:37 +0800

    reformat file

--- agentserver/server/cron_task.py
+++ agentserver/server/cron_task.py
@@ -1,6 +1,3 @@
-import datetime
-
-import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from django_apscheduler.jobstores import DjangoJobStore
