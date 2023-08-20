85183de55f39453aa6c3c866a25f232560479127
may_i21 <may_i21@163.com>
2023-08-20 10:55:24 +0800

    implement auto scan by health check

--- agentserver/server/cron_task.py
+++ agentserver/server/cron_task.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
@@ -16,23 +18,28 @@ http_util = HttpUtil()
 
 # cron health check
 def health_monitor():
-    host_status_records = HostStatusRecord.objects.exclude(status=DeployHostStatus.OFFLINE.value).exclude(status=DeployHostStatus.STOP.value)
+    host_status_records = HostStatusRecord.objects.exclude(status=DeployHostStatus.OFFLINE.value).exclude(
+        status=DeployHostStatus.STOP.value)
     for ins in host_status_records:
         # TODO test use local host
         # production use vm ip
         health_check_url = "http://{}:{}/health".format('127.0.0.1', ins.port)
-        print(health_check_url)
         resp = http_util.req(RequestInfo.METHOD_GET,
-                      health_check_url,
-                      data=None)
-        print(resp.content, resp.status_code)
-        if resp.status_code == 200:
-            ins.status = DeployHostStatus.ONLINE.value
-        else:
-            ins.status = DeployHostStatus.UNKNOWN.value
-        ins.save()
+                             health_check_url,
+                             data=None)
+        from datetime import datetime
 
+        # 获取当前时间
+        current_time = datetime.now()
+        # 格式化并打印当前时间
+        formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+
+        print(health_check_url, resp.content, resp.status_code, formatted_time)
+        if resp.status_code != 200:
+            ins.delete()
 
 scheduler.add_job(health_monitor,
-                  trigger=IntervalTrigger(seconds=60),
+                  trigger=IntervalTrigger(seconds=10),
+                  id='health_monitor',
+                  replace_existing=True
                   )
